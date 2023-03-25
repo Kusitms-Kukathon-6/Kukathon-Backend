2cac4a35d7a8f8de6d6768ca9391d0832d2442fe
Jinwoo Chang <jenny010925@naver.com>
2023-03-25 21:09:19 +0900

    [FEAT] 지하철역명에서 지하철 코드 변환 기능 구현

--- src/main/java/com/example/kusitsm/subway/Service/SubwayService.java
+++ src/main/java/com/example/kusitsm/subway/Service/SubwayService.java
@@ -1,4 +1,15 @@
 package com.example.kusitsm.subway.Service;
 
+import com.example.kusitsm.subway.Dto.Res.SubwayCodeReturn;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.json.simple.parser.ParseException;
+import org.springframework.http.ResponseEntity;
+
 public interface SubwayService {
+    SubwayCodeReturn getSubwayCode(String startStation, String endStation) throws JsonProcessingException, ParseException;
+
+    String[] fetch(String url) throws JsonProcessingException, ParseException;
+
+
+
 }
